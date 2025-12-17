85801386f0d5c8f0fc8674d684c597376564585f
Ihorkravc <kravcov1991@outlook.com>
2025-12-17 17:48:59 +0200

    Task 16_ player, class and health

---
+++ С++Project/С++Project/Task16.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() {
+	cout << "Player name is: "<<"Hector" <<endl;
+	cout << "Health: " <<"100" << endl;
+    cout << "Class: " << "humanity" << endl;
+	return 0;
+}
