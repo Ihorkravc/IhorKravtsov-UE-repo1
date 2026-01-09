8c0785d47d32f8f73fcc96cc85b52b4f1442b391
Ihorkravc <kravcov1991@outlook.com>
2026-01-09 19:26:11 +0200

    Task 19
    Додав число N , яка рахує скільки типів юнітів ми можемо вписати, а також калькулятор,
    який рахує який з типів має більше одиниць

---
+++ С++Project/С++Project/Task19.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+#include <map>;
+using namespace std;
+
+
+
+// Оголошення змінних
+int N;
+map<int,int>MYclan;
+
+
+int main() {
+
+	// ввести N
+    cout << "Write number N:";
+    cin >>   N;
+
+
+
+	for (int i = 0; i < N; i++)
+	{
+		int type, add;
+        cout << "Write number of type:";
+		cin >> type >> add;
+		MYclan[type] += add;
+
+	}
+    int maxType, minType;
+    int maxCount, minCount;
+    bool first = true;
+
+    for (auto pair : MYclan) {
+        int type = pair.first;
+        int count = pair.second;
+
+        if (first) {
+            maxType = minType = type;
+            maxCount = minCount = count;
+            first = false;
+        }
+        else {
+            if (count > maxCount) {
+                maxCount = count;
+                maxType = type;
+            }
+
+            if (count < minCount) {
+                minCount = count;
+                minType = type;
+            }
+        }
+    }
+   
+    cout << "Min count: " << minCount << endl;
+    cout << "Max count: " << maxCount << endl;
+
+    return 0; }
+
+	
+
